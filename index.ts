e6792b546f10fc7883dab943bf800ac6203fc3e5
Daniel S <dsmathers91@outlook.com>
2022-09-24 17:20:19 -0700

    parse in greyscale and color instead of just one

--- index.ts
+++ index.ts
@@ -5,11 +5,10 @@ import { WoolColors } from './colors/wool/woolColors';
 import parseImage from './parseImage';
 
 let pixelArray : WoolColors[][] = [];
-if(process.argv[3].trim().toLocaleLowerCase() === "false") {
-    pixelArray= parseImage(process.argv[2], false);
-} else {
-    pixelArray = parseImage(process.argv[2], true);
-}
+
+const colorArray : WoolColors[][] = parseImage(process.argv[2], true);
+const greyscaleArray : WoolColors[][] = parseImage(process.argv[2], false);
+
 
 const subscribeToPlayerMessages = {
     "header":{
@@ -49,16 +48,23 @@ const wss = new ws.Server({
 
 
 function messageToDrawCommand ( socket: any, message : string[] )  {
+    console.log(message);
     let x = Number(message[2]);
     let y = Number(message[3]);
     let z = Number(message[4]);
-    let ignoreWhitespace = message[5] === "true" ? true : false; 
+    let isColor = message[5] === 'true' ? true : false;
+    let ignoreWhitespace = message[6] === "true" ? true : false; 
     if(message[1] === "h") {
-        drawHorizontal(socket, pixelArray, x, y, z, ignoreWhitespace);
+        drawHorizontal(socket, 
+                        isColor ? colorArray : greyscaleArray, 
+                        x, 
+                        y, 
+                        z, 
+                        ignoreWhitespace);
     }
 
     else if ( message[1] === "v") {
-        drawVerticle(socket, pixelArray, x, y, z, ignoreWhitespace);
+        drawVerticle(socket, isColor ? colorArray : greyscaleArray, x, y, z, ignoreWhitespace);
     }
 }
 
